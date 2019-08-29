da1558ce89eed945dec20dfa50d35467b99eff4a
Greg Jordan <gregljordan@gmail.com>
2019-08-28 20:18:00 -0400

    setting up postgres

--- knexfile.js
+++ knexfile.js
@@ -1,30 +1,37 @@
 // Update with your config settings.
 
+localPbConnection = {
+  host: "localhost",
+  database: "Database",
+  user: process.env.DB_USERS,
+  password: process.env.DB_PASS
+};
+
 module.exports = {
 
   development: {
     client: 'sqlite3',
     useNullAsDefault: true, 
-    pool: {
-      afterCreate: (conn, done) => {
-        conn.run('PRAGMA foreign_keys = ON', done);
-      }
-    },
+    // pool: {
+    //   afterCreate: (conn, done) => {
+    //     conn.run('PRAGMA foreign_keys = ON', done);
+    //   }
+    // },
     connection: {
       filename: './database/reads.db3'
     },
     migrations: {
-      directory: './database/migrations'
+      directory: './database/migrations',
+      tableName: "dbmigrations",
     },
     seeds: {
       directory: './database/seeds'
     },
-   
   },
 
   production: {
     client: 'pg',
-    connection: process.env.DATABASE_URL,
+    connection: process.env.DATABASE_URL || localPbConnection,
     migrations: {
       directory: './database/migrations'
     },
@@ -32,6 +39,7 @@ module.exports = {
       directory: './database/seeds'
     },
   },
+
   testing: {
     client: 'sqlite3',
     connection: {
